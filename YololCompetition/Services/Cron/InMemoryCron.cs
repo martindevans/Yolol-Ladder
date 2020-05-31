f7bb2b3c09b70379a953ed49072c8ab8b6e2819f
Martin Evans <martindevans@gmail.com>
2020-06-01 00:58:54 +0100

    - Modified `ICron` to work in a new way - supporting cancellation tokens and variable length delays.  - Added a shim so old uses of `ICron` continue working as normal

--- YololCompetition/Services/Cron/InMemoryCron.cs
+++ YololCompetition/Services/Cron/InMemoryCron.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace YololCompetition.Services.Cron
@@ -6,21 +7,40 @@ namespace YololCompetition.Services.Cron
     public class InMemoryCron
         : ICron
     {
-        public void Schedule(TimeSpan period, TimeSpan initialDelay, uint maxRuns, Func<Task<bool>> run)
+        public Task Schedule(TimeSpan period, TimeSpan initialDelay, CancellationToken token, Func<CancellationToken, Task<bool>> run)
         {
-            Task.Run(async () => {
+            return Task.Run(async () => {
 
-                await Task.Delay(initialDelay);
+                await Task.Delay(initialDelay, token);
 
-                for (uint i = 0; i < maxRuns; i++)
+                while (!token.IsCancellationRequested)
                 {
-                    var cont = await run();
+                    var cont = await run(token);
                     if (!cont)
                         break;
 
-                    await Task.Delay(period);
+                    await Task.Delay(period, token);
                 }
-            });
+                
+            }, token);
+        }
+
+        public Task Schedule(TimeSpan initialDelay, CancellationToken token, Func<CancellationToken, Task<TimeSpan?>> run)
+        {
+            return Task.Run(async () => {
+
+                await Task.Delay(initialDelay, token);
+
+                while (!token.IsCancellationRequested)
+                {
+                    var delay = await run(token);
+                    if (delay == null)
+                        break;
+
+                    await Task.Delay(delay.Value, token);
+                }
+                
+            }, token);
         }
     }
 }
