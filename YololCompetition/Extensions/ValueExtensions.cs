4f554a6ef462f0e9eb3d77014c08c21c5781068a
Martin Evans <martindevans@gmail.com>
2021-12-09 02:19:06 +0000

    Improved display of very long strings

--- YololCompetition/Extensions/ValueExtensions.cs
+++ YololCompetition/Extensions/ValueExtensions.cs
@@ -14,9 +14,18 @@ namespace YololCompetition.Extensions
             {
                 { Type: Type.Number } => value.Number.ToString(),
                 { Type: Type.String, String: { Length: <= StrLengthLimit } } => $"\"{value.String}\"",
-                { Type: Type.String } => $"\"{value.ToString()[..StrLengthLimit]}\" ...({value.String.Length - StrLengthLimit} more characters trimmed)",
+                { Type: Type.String } => TrimLongString(value.String.ToString()),
                 _ => throw new FormatException($"Cannot format value of type `{value.Type}`"),
             };
+
+            static string TrimLongString(string value)
+            {
+                var left = value[..32];
+                var right = value[^32..];
+                var count = left.Length + right.Length;
+
+                return $"{left} ...({value.Length - count} characters omitted)... {right}";
+            }
         }
     }
 }
